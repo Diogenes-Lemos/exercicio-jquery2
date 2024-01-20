db9bce4ca4142f09018474a0671eabc1b1ecbc48
Lahar Alharzed <diogenes.lemos1@gmail.com>
2024-01-20 11:25:42 -0300

    versão inicial

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#CPF").mask("999-999-999-99");
+    $("#CEP").mask("99999-999");
+    $("#telefone").mask("(99) 9999-9999")
+})
+
+
+
+
+/*alert("até aqui tudo certo!")*/
